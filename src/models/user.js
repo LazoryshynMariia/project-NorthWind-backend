e7703fc8ee99de41ca0c55119b75c16436b0904f
Nina Aksenich <maruaninashved1488@gmail.com>
2026-07-05 15:53:43 +0300

    feat(auth): create user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,33 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose';
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
 
 export const UserModel = model('user', userSchema);
