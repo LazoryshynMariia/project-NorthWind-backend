684d46aa92b05de83f51a7904c3f49ee7035dbaf
Yelyzaveta Vecherovska <vecheli0306@gmail.com>
2026-07-08 22:18:09 +0300

    add users travellers exports

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,12 +1,14 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
   getTravellers,
   getTopTravellers,
-} from "../controllers/users/getTravellers.js";
+  getTravellerById,
+} from '../controllers/users/index.js';
 
 const usersRouter = Router();
 
-usersRouter.get("/travellers/top", getTopTravellers);
-usersRouter.get("/travellers", getTravellers);
+usersRouter.get('/travellers/top', getTopTravellers);
+usersRouter.get('/travellers', getTravellers);
+usersRouter.get('/travellers/:travellerId', getTravellerById);
 
 export default usersRouter;
