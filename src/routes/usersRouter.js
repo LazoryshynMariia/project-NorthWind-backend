cada4acf7ee96c057f835d40aabc55539edcbc3b
Ivan <dziamko02@gmail.com>
2026-07-08 11:09:10 +0300

    restore traveller by id route lost during merge

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -3,10 +3,12 @@ import {
   getTravellers,
   getTopTravellers,
 } from "../controllers/users/getTravellers.js";
+import { getTravellerById } from "../controllers/users/getTravellerById.js";
 
 const usersRouter = Router();
 
 usersRouter.get("/travellers/top", getTopTravellers);
 usersRouter.get("/travellers", getTravellers);
+usersRouter.get("/travellers/:travellerId", getTravellerById);
 
 export default usersRouter;
