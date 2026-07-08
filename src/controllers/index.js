4dd44f25a3fe0cbcd867044fa2692175051c63ca
Illia <mddg354@gmail.com>
2026-07-08 13:05:53 +0300

    add popular stories endpoint

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,10 @@
 import { registerUser } from "./auth/registerUser.js";
+import { getPopularStories } from "./stories/getPopularStories.js";
 
 export const auth = {
   registerUser,
+};
+
+export const stories = {
+  getPopularStories,
 };
--- src/routes/storiesRouter.js
+++ src/routes/storiesRouter.js
@@ -3,8 +3,11 @@ import { celebrate } from "celebrate";
 import { createStorySchema } from "../validations/articlies/addStoryValidation.js";
 import { addStory } from "../controllers/stories/storiesController.js";
 import { upload } from "../middleware/multer.js";
+import { getPopularStories } from "../controllers/stories/getPopularStories.js";
 
 const storiesRouter = Router();
+
+storiesRouter.get("/popular", getPopularStories);
 
 storiesRouter.post(
   "",
