42c091065a81f001f6747f591ec32d0453bbd7f2
Lazoryshyn Mariia <106499581+LazoryshynMariia@users.noreply.github.com>
2026-07-09 06:32:41 +0300

    Update updateTheme.js

--- src/controllers/auth/updateTheme.js
+++ src/controllers/auth/updateTheme.js
@@ -1,21 +1,25 @@
 import createHttpError from "http-errors";
 import { UserModel } from "../../models/user.js";
 
-export const updateTheme = async (req, res) => {
-  const { _id: userId } = req.user;
-  const { theme } = req.body;
+export const updateTheme = async (req, res, next) => {
+  try{
+    const { _id: userId } = req.user;
+    const { theme } = req.body;
 
-  const updatedUser = await UserModel.findByIdAndUpdate(
-    userId,
-    { theme },
-    { new: true },
-  );
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { theme },
+      { new: true },
+    );
 
-  if (!updatedUser) {
-    throw createHttpError(404, "User not found");
+    if (!updatedUser) {
+     throw createHttpError(404, "User not found");
+    }
+
+     res.status(200).json({
+      theme: updatedUser.theme,
+    });
+  } catch (error) {
+    next(error)
   }
-
-  res.status(200).json({
-    theme: updatedUser.theme,
-  });
 };
